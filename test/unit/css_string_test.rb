097c5a8c287b3346e0eceb28db8e2238dc8176a9
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-08 20:15:49 -0400

    First test passed (method '+').

--- test/unit/css_string_test.rb
+++ test/unit/css_string_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class CssStringTest < ActiveSupport::TestCase
 
   test "+" do
-    assert_equal 'a + b', CssString.new('a')+'b'
+    assert_equal 'ab', CssString.new('a')+'b'
   end
+
+=begin
+  test "qq" do
+    assert_equal '', CssString.new('a').qq'b'
+  end
+=end
 
 end
