f0d4f3c6a2b893a39a31219caba99cf9c88efc99
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-11 13:52:27 -0400

    Add TestBed to app name prior to plugin.

--- app/models/css_selector.rb
+++ app/models/css_selector.rb
@@ -1,6 +1,6 @@
 #@+leo-ver=5-thin
 #@+node:georgesawyer.20110806135445.1334: * @file css_selector.rb
-module CssSelector
+module CssSelector2
 
   %w[ A BODY DIV FORM HEAD HTML IMG INPUT LABEL SCRIPT STYLE TABLE TITLE
       ].each do |e|
