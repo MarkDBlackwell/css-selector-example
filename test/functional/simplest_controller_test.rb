dbbc180c8eb7a6101904a782b0779154899c7424
Mark D. Blackwell <markdblackwell01@gmail.com>
2011-08-16 10:27:12 -0400

    Toward plugin view test.

--- test/functional/simplest_controller_test.rb
+++ test/functional/simplest_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SimplestControllerTest < ActionController::TestCase
 
-  include CssSelector2
+  include CssStringNamespace
 
   test "scripts div..." do
 # Should include certain script tags in order:
--- test/unit/helpers/simplest_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SimplestHelperTest < ActionView::TestCase
-end
